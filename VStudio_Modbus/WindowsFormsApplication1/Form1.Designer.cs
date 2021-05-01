999e69ea235d7474b5c9db9c4d5a3372467c1e1d
Vinicius Ramon Reis <55250084+viniciusramon@users.noreply.github.com>
2021-04-30 22:07:58 -0300

    Add files via upload

---
+++ VStudio_Modbus/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.setTemp = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ATemp = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.PJ1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Setpoint = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.ReadBufferSize = 50;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.DataReceivedHandler);
+            // 
+            // setTemp
+            // 
+            this.setTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.setTemp.Location = new System.Drawing.Point(194, 98);
+            this.setTemp.Name = "setTemp";
+            this.setTemp.Size = new System.Drawing.Size(90, 31);
+            this.setTemp.TabIndex = 1;
+            this.setTemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.setTemp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 200;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Conectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tensão:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(303, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "V";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(303, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(122, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Temp:";
+            // 
+            // ATemp
+            // 
+            this.ATemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ATemp.Location = new System.Drawing.Point(194, 135);
+            this.ATemp.Name = "ATemp";
+            this.ATemp.Size = new System.Drawing.Size(90, 31);
+            this.ATemp.TabIndex = 5;
+            this.ATemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(303, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "C";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(303, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "V";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(122, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "PJ1:";
+            // 
+            // PJ1
+            // 
+            this.PJ1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PJ1.Location = new System.Drawing.Point(194, 172);
+            this.PJ1.Name = "PJ1";
+            this.PJ1.Size = new System.Drawing.Size(90, 31);
+            this.PJ1.TabIndex = 8;
+            this.PJ1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(235, 19);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(69, 20);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Setpoint";
+            // 
+            // Setpoint
+            // 
+            this.Setpoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Setpoint.Location = new System.Drawing.Point(307, 14);
+            this.Setpoint.Name = "Setpoint";
+            this.Setpoint.Size = new System.Drawing.Size(90, 31);
+            this.Setpoint.TabIndex = 10;
+            this.Setpoint.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 40);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 274);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Setpoint);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.PJ1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ATemp);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.setTemp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox setTemp;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ATemp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox PJ1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Setpoint;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VStudio_Modbus/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public string SValor { get; set; }
+        public int Voltage, Temperature, InputPJ1;
+        public int rcnt;
+        public int rec_step;
+        public byte[] rec = { 0, 0, 0, 0, 0, 0, 0, 0 };
+        public int reqAddr = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        private void DataReceivedHandler(object sender,
+         System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            byte[] data = new byte[serialPort1.BytesToRead];
+            serialPort1.Read(data, 0, data.Length);
+
+            if ((data[0] == 0x05) && (data.Length>6))
+            {
+                if (reqAddr == 0)
+                {
+                    Voltage = (int)data[3] * 256 + data[4];
+                }
+                else if (reqAddr == 2)
+                {
+                    Temperature = (int)data[3] * 256 + data[4];
+                }
+                else if (reqAddr == 4)
+                {
+                    InputPJ1 = (int)data[3] * 256 + data[4];
+                }
+            } 
+   }
+   
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) || (e.KeyChar == '.') || (e.KeyChar == ','))
+            {
+
+            }
+            else
+            {
+                e.Handled = e.KeyChar != (char)Keys.Back;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            float FVoltage;                             // auxiliar para visualizar casas decimais
+            reqAddr = (reqAddr == 4) ? 0 : reqAddr + 2;     // endereco da variavel a ser lida : 0 , 2 e 4;
+            request();                                  // send request
+            FVoltage = (float)Voltage / 1000;       
+            setTemp.Text = FVoltage.ToString();
+            ATemp.Text = Temperature.ToString();
+            PJ1.Text = InputPJ1.ToString();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.Open();
+            }
+
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        private void request()
+        {
+            byte[] envia = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            int crcValue;
+            int crc0, crc1;
+
+            envia[0] = 05;                      // endereco Modbus
+            envia[1] = 03;                      // le parametro
+            envia[2] = 00;                      // endereco 
+            envia[3] = (byte)reqAddr;           // endereco
+            envia[4] = 00;                      // quantidade
+            envia[5] = 01;                      // quantidade
+            crcValue = crc(envia, 0, 6);
+            crc0 = (crcValue & 0xFF00) >> 8;
+            envia[6] = (byte)crc0;              // CRC
+            crc1 = (crcValue & 0x00FF);
+            envia[7] = (byte)crc1;              // CRC
+
+            serialPort1.Write(envia, 0, 8);
+            
+        }
+
+        private int crc(byte[] buf, int start, int cnt) 
+        {
+            int i, j;
+            int temp, temp2;
+            int flag;
+
+            temp = 0xFFFF;
+
+            for (i = start; i < cnt; i++)
+            {
+                temp = temp ^ buf[i];
+
+                 for (j = 1; j <= 8; j++)
+                 {
+                    flag = temp & 0x0001;
+                    temp = temp >> 1;
+                    if (flag==1)
+                    {
+                        temp = temp ^ 0xA001;
+                    }
+                 }
+           }
+        
+
+            /* Reverse byte order. */
+            temp2 = temp >> 8;
+            temp = (temp << 8) | temp2;
+            temp &= 0xFFFF;
+
+            return (temp);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] envia = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            int crcValue;
+            int crc0, crc1;
+            int valor = 0;
+            Int32.TryParse(Setpoint.Text, out valor);
+
+            envia[0] = 05;                      // endereco Modbus
+            envia[1] = 06;                      // le parametro
+            envia[2] = 00;                      // endereco 
+            envia[3] = 08;                      // endereco
+            envia[4] = (byte)((valor & 0xFF00) >>8);    // valor
+            envia[5] = (byte)(valor & 0x00FF);          // valor
+            crcValue = crc(envia, 0, 6);
+            crc0 = (crcValue & 0xFF00) >> 8;
+            envia[6] = (byte)crc0;              // CRC
+            crc1 = (crcValue & 0x00FF);
+            envia[7] = (byte)crc1;              // CRC
+
+            serialPort1.Write(envia, 0, 8);
+        }
+
+
+
+
+    }
+}
